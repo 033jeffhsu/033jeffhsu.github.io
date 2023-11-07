084f72185ed068093227eab38f3326467b929242
Jeff Hsu <33831637+033jeffhsu@users.noreply.github.com>
2023-11-06 19:50:43 -0800

    change scripts directory

--- main.js
+++ main.js
@@ -5,14 +5,30 @@ var content = document.getElementById("content");
 var button = document.getElementById("btn");
 var list = document.getElementById("list");
 var upload = document.getElementById("upload");
+var image = document.getElementById("image");
 
+const imagearr = [];
+
+upload.addEventListener("click", function (){
+    imagearr.push(image.value);
+    upload.value = "";
+})
 button.addEventListener("click", function(){
+
     list.innerHTML +=
         `<div class="article">
             <h2>${title.value}</h2>
             <p>${content.value}</p>
-        </div>`;
+        `;
+    for (let i = 0; i < imagearr.length; i++){
+
+        list.innerHTML += `
+        <img src="${imagearr[i]}" class="event" alt="event png">
+        `;
+    }
+    list.innerHTML += `</div>`;
     title.value = "";
     content.value ="";
 });
+
 
